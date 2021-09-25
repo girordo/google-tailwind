7412df549ac8ded58d62c662a99e57df5070caef
girordo <tsgiroldo@gmail.com>
2021-09-25 16:21:41 -0300

    :sparkles: | Started the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <section className="min-h-screen w-full flex items-center justify-center">
+      <h1 className="text-4xl font-bold font-sans">Google with Tailwind</h1>
+    </section>
   );
-}
+};
 
 export default App;
