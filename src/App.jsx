9bee81dc15672d16a6fab905617067eeb351c989
girordo <tsgiroldo@gmail.com>
2021-09-25 16:38:56 -0300

    :sparkles: | Header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 const App = () => {
   return (
-    <section className="min-h-screen w-full flex items-center justify-center">
-      <h1 className="text-4xl font-bold font-sans">Google with Tailwind</h1>
+    <section className="min-h-screen w-full">
+      <header className="flex justify-end">
+        <nav>
+          <a className="inline-block py-3 px-4">Gmail</a>
+          <a className="inline-block py-3 px-4">Imagens</a>
+          <a className="inline-block py-3 px-4">Google Apps</a>
+          <img
+            src="https://lh3.googleusercontent.com/ogw/ADea4I5fM47yd-ZLmGipUXnd5OdeyYwbEjwnCHmk_PvnuA=s32-c-mo"
+            className="rounded-full inline-block py-3 px-4"
+          />
+        </nav>
+      </header>
+      <main></main>
+      <footer></footer>
     </section>
   );
 };
