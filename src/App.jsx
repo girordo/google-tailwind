011d3dd1ef2f147176eddc62cf1280daba5dc5a0
girordo <tsgiroldo@gmail.com>
2021-09-27 14:45:27 -0300

    :bug: | fixing a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <section>
+    <section className="flex flex-col justify-evenly">
       <header className="flex justify-end">
         <nav>
           <a className="inline-block py-3 px-4 font-light">Gmail</a>
@@ -11,14 +11,14 @@ const App = () => {
           />
         </nav>
       </header>
-      <main className="h-screen w-full flex flex-col justify-center items-center">
+      <main className="min-h-screen w-full flex flex-col justify-center items-center">
         <img
           src="https://www.google.com/logos/doodles/2021/googles-23rd-birthday-6753651837109087-law.gif"
           className="mb-10"
         />
-        <input className="w-1/2 h-10 mb-10 rounded-3xl border-2 hover:shadow-md md:w-1/4 lg:w-1/4 xl:1/4 2xl:1/4" />
+        <input className="w-1/2 h-10 mb-10 rounded-3xl px-2 border-2 hover:shadow-md md:w-1/4 lg:w-1/4 xl:1/4 2xl:1/4" />
         <section className="flex flex-row justify-evenly mb-10">
-          <button className="bg-gray-200 py-2 px-4 font-light text-sm border hover:shadow-md ">
+          <button className="bg-gray-200 py-2 px-4 mr-4 font-light text-sm border hover:shadow-md ">
             Pesquisa Google
           </button>
           <button className="bg-gray-200 py-2 px-4 font-light text-sm border hover:shadow-md ">
@@ -26,7 +26,7 @@ const App = () => {
           </button>
         </section>
       </main>
-      <footer>
+      <footer className="w-full bottom-0">
         <section className="bg-gray-200">
           <a className="inline-block py-3 px-4 text-gray-600 text-sm">Brasil</a>
         </section>
